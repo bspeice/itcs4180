ecb13bc4ed98d29245bb7cefe152de0614b3b2dc
DjBushido <bspeice.nc@gmail.com>
2014-02-28 20:34:01 -0500

    Buttons now launch the correct activities Only thing left to do is the photo activities themselves.

--- InClass3/src/edu/uncc/itcs4180/hw4/MainActivity.java
+++ InClass3/src/edu/uncc/itcs4180/hw4/MainActivity.java
@@ -2,7 +2,9 @@ package edu.uncc.itcs4180.hw4;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +19,14 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void onClick(View v) {
+		// One of our buttons has been clicked, let's start the activity and go!
+		if (v.getId() == R.id.btnAsync)
+			startActivity(new Intent(this, PhotoActivity.class));
+		else
+			startActivity(new Intent(this, PhotoThread.class));
 	}
 
 }
