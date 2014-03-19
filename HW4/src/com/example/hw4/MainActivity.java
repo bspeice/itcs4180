0e90b852c9864fc0767da9786c4e65c84c6908e6
DjBushido <bspeice.nc@gmail.com>
2014-03-18 20:33:51 -0400

    Commit initial Homework 4

---
+++ HW4/src/com/example/hw4/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.hw4;
+
+/*
+ * Bradlee Speice, Brandon Rodenmayer
+ * ITIS 4180
+ * In Class 3
+ * MainActivity.java
+ */
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public void onClick(View v) {
+		// One of our buttons has been clicked, let's start the activity and go!
+		if (v.getId() == R.id.btnAsync)
+			startActivity(new Intent(this, PhotoActivity.class));
+		else
+			startActivity(new Intent(this, PhotoThread.class));
+	}
+
+}
