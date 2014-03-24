cef01d01e0834b374c55a8efcfe3af269fb97efe
DjBushido <bspeice.nc@gmail.com>
2014-03-24 10:02:07 -0400

    Initial layout and code skeleton for ImageViewer

--- HW4/src/com/example/hw4/ImageViewerActivity.java
+++ HW4/src/com/example/hw4/ImageViewerActivity.java
@@ -1,15 +1,27 @@
 package com.example.hw4;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.view.Menu;
+import android.view.View;
 
 public class ImageViewerActivity extends Activity {
+	
+	int urlsId;
+	String[] urls;
+	int currentIndex;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_image_viewer);
+		
+		Bundle extras = getIntent().getExtras();
+		urlsId = extras.getInt("urls");
+		urls = getResources().getStringArray(urlsId);
+		currentIndex = extras.getInt("index");
 	}
 
 	@Override
@@ -17,6 +29,36 @@ public class ImageViewerActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.image_viewer, menu);
 		return true;
+	}
+	
+	public void onClickPrev(View v) {
+		
+	}
+		
+	public void onClickNext(View v) {
+		
+	}
+	
+	public void onClickBack(View v) {
+		finish();
+	}
+	
+	public void displayImage(int indexToDisplay) {
+		
+		
+	}
+	
+	private class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
+		@Override
+		protected Bitmap doInBackground(String... params) {
+			return null;
+		}
+		
+		@Override
+		protected void onPostExecute(Bitmap result) {
+			// TODO Auto-generated method stub
+			super.onPostExecute(result);
+		}
 	}
 
 }
