950a188e301b04feb6ed0f6f3a56bed2cd6711e0
Bradlee Speice <bspeice.nc@gmail.com>
2014-04-01 19:26:17 -0400

    Don't actually need the templates

--- UNCCScavenger-Server/UNCCScavenger_Server/scavenger/models.py
+++ UNCCScavenger-Server/UNCCScavenger_Server/scavenger/models.py
@@ -5,5 +5,6 @@ class Location(models.Model):
 	key = models.CharField(max_length=32)
 	name = models.CharField(max_length=32)
 	riddle = models.CharField(max_length=512)
+	riddle_image_url = models.URLField()
 	location = models.CharField(max_length=16)
 	result = models.TextField()
