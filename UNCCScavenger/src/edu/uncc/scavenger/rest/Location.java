c41c2bf4b92557e6bb5d07968b8ddad9d9477dee
DjBushido <bspeice.nc@gmail.com>
2014-04-05 17:02:07 -0400

    Add initial Location for use in server communication

--- UNCCScavenger/src/edu/uncc/scavenger/rest/Location.java
+++ UNCCScavenger/src/edu/uncc/scavenger/rest/Location.java
@@ -1,5 +1,41 @@
 package edu.uncc.scavenger.rest;
 
 public class Location {
-
+	
+	private int id;
+	private String name;
+	private String riddle;
+	private double locationLong;
+	private double locationLat;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getRiddle() {
+		return riddle;
+	}
+	public void setRiddle(String riddle) {
+		this.riddle = riddle;
+	}
+	public double getLocationLong() {
+		return locationLong;
+	}
+	public void setLocationLong(double locationLong) {
+		this.locationLong = locationLong;
+	}
+	public double getLocationLat() {
+		return locationLat;
+	}
+	public void setLocationLat(double locationLat) {
+		this.locationLat = locationLat;
+	}
 }
