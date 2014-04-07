7a7ed266f1e41cebcbc7377eacf8a6281cfaea66
DjBushido <bspeice.nc@gmail.com>
2014-04-07 09:06:52 -0400

    Add the constants needed for the Twitter client

---
+++ HW5/src/edu/uncc/itcs4180/hw5/TwitterClient.java
@@ -0,0 +1,13 @@
+package edu.uncc.itcs4180.hw5;
+
+import java.net.URL;
+
+public class TwitterClient {
+	
+	private final String API_KEY = "HuEbvsXOxQsKVmTneyilVtsV6";
+	private final String API_SECRET = "ZIJqmKEGoQUKLCIit15SKz6XIl4PP1xgDm1jVCIBSDhmImzFqk";
+	
+	private final String URL_TOKEN = "https://api.twitter.com/oauth2/token";
+	private final String URL_API = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=";
+
+}
