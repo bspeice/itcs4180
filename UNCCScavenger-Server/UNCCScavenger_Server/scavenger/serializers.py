6d088e24d317dc773555ae02aedd58ed23712ad6
Bradlee Speice <bspeice.nc@gmail.com>
2014-04-19 23:20:14 -0400

    Add the riddle_image_url to the serializer

--- UNCCScavenger-Server/UNCCScavenger_Server/scavenger/serializers.py
+++ UNCCScavenger-Server/UNCCScavenger_Server/scavenger/serializers.py
@@ -5,7 +5,8 @@ from models import Location
 class LocationListSerializer(ModelSerializer):
 	class Meta:
 		model = Location
-		fields = ('id', 'name', 'riddle', 'location_long', 'location_lat')
+		fields = ('id', 'name', 'riddle', 'location_long', 'location_lat',
+					'riddle_image_url')
 
 class LocationResultSerializer(ModelSerializer):
 	class Meta:
