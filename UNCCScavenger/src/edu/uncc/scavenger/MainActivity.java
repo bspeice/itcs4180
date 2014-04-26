a330ab9155e1d41197ef7f8f5f8567970a89e33c
DjBushido <bspeice.nc@gmail.com>
2014-04-26 09:37:13 -0400

    String compare doesn't work the way you think it does...

--- UNCCScavenger/src/edu/uncc/scavenger/MainActivity.java
+++ UNCCScavenger/src/edu/uncc/scavenger/MainActivity.java
@@ -154,7 +154,7 @@ public class MainActivity extends Activity {
 				holder.name = (TextView)v.findViewById(R.id.txtName);
 				holder.riddle = (TextView)v.findViewById(R.id.txtRiddle);
 				
-				if(locations.get(position).getKey()!=null)
+				if(!locations.get(position).getKey().equals(""))
 				{
 					holder.imgFound.setImageResource(R.drawable.checkbox_checked);
 				}
